6eae09ca7386083a23f93d967c8aab53182f4d60
Iluschenko Kirill <k.iluschenko@gmail.com>
2018-01-27 00:32:04 +0300

    Задача 1. Слайдер

---
+++ browser/slider/js/slider.js
@@ -0,0 +1,19 @@
+var imgArray = [
+'airmax-jump.png',
+'airmax-on-foot.png',
+'airmax-playground.png',
+'airmax-top-view.png',
+'airmax.png']
+
+const slider = document.getElementById('slider');
+let step = 0;
+
+function showSlider(){
+	if (step === imgArray.length) {
+		step = 0;
+	}
+	slider.src = `i/${imgArray[step]}`;
+	step += 1;
+}
+
+setInterval(showSlider, 5000);
