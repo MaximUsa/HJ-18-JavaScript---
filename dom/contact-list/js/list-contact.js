c5164a2751976e54e80d65b5032367f387d3607c
Iluschenko Kirill <k.iluschenko@gmail.com>
2018-02-09 00:23:17 +0300

    Задача 1. Список контактов.

--- dom/contact-list/js/list-contact.js
+++ dom/contact-list/js/list-contact.js
@@ -4,16 +4,14 @@ const contactsList = document.querySelector('.contacts-list'); //ищем пер
 contactsList.innerHTML = ''; // очищаем 
 
 function addContact(contactJson) {
-	for(const contact of contactJson){ //перебираем элементы 
+	for(const contact of contactJson) { //перебираем элементы 
 		contactsList.appendChild(document.createElement('li'));//создаем новый тег li и добавляем его как дочерний в блок тега с классом .contacts-list
 		const contactsDataNew = document.querySelectorAll('.contacts-list > li'); //коллекция тегов li 
 		let index = contactJson.indexOf(contact); //индекс элемента в объекте 
-		contactsDataNew[index].innerHTML =`<strong>${contact.name} </strong>`;	 //добавляем содержимое в блок тег li	
+		contactsDataNew[index].innerHTML =`<strong> ${contact.name} </strong>`;	 //добавляем содержимое в блок тег li	
 		contactsDataNew[index].dataset.phone = contact.phone; //добавляем атрибуты 
 		contactsDataNew[index].dataset.email = contact.email;	
 	}
-
 }
 
 addContact(parseCont);
-
