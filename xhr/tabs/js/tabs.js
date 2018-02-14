3ce31f82881fde8c459199ef2c7f4754f0b53cbf
Iluschenko Kirill <k.iluschenko@gmail.com>
2018-02-14 23:10:26 +0300

    Задача 2. Табы

---
+++ xhr/tabs/js/tabs.js
@@ -0,0 +1,44 @@
+
+const request = new XMLHttpRequest();
+request.addEventListener('load', onLoad);
+request.open('GET', 'components/email-tab.html');
+request.send();
+
+const content = document.querySelector('#content')
+			tabs = document.querySelectorAll('nav a')
+			preload = document.querySelector('#preloader');
+
+Array.from(tabs).forEach(tab => {
+		tab.addEventListener('click', defaultEvent);
+	});
+
+
+function defaultEvent(event){
+	for (let tab of tabs) {
+    tab.classList.remove('active');
+  }
+  this.classList.add('active');      
+  event.preventDefault();
+	request.addEventListener('loadstart', onLoadStart);
+  request.addEventListener('load', onLoad);
+	request.addEventListener('loadend', onLoadEnd);
+	request.open('GET', this.href);
+  request.send();
+}
+
+function onLoad() {
+	content.innerHTML = request.responseText;
+}
+
+function onLoadSms() {
+ 	content.innerHTML = requestSms.responseText;
+}
+
+
+function onLoadStart() {
+	preload.classList.remove('hidden');
+}
+
+function onLoadEnd() {
+	preload.classList.add('hidden');
+}
