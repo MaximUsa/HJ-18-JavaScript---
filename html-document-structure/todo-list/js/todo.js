8609c8b316c5663c31d4f6b9a1674498cc14f515
Kirill <k.iluschenko@mail.ru>
2018-02-20 21:14:42 +0300

    Задача 2. список дел

---
+++ html-document-structure/todo-list/js/todo.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const main = document.querySelector('.todo-list');
+const done = main.querySelector('.done');
+const undone = main.querySelector('.undone');
+const inputs = main.querySelectorAll('input');
+
+	function todo() {
+		console.log(this.hasAttribute('checked'))
+
+		if (this.hasAttribute('checked')) {
+			undone.appendChild(this.parentElement)
+			this.removeAttribute('checked');
+		} else {
+			done.appendChild(this.parentElement);
+			this.setAttribute('checked', true);
+		}
+	}
+Array.from(inputs).forEach(input => input.addEventListener('click',todo));
