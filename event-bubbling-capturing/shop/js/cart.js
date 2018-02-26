99435edf4c491c775651efcb36b46c088dfa54dc
Kirill <k.iluschenko@mail.ru>
2018-02-26 21:42:10 +0300

    Задача 3. Добавить в корзину

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,11 @@
 'use strict';
+
+list.addEventListener('click', checkCart);
+
+function checkCart(event) {
+	if ((event.target.tagName === 'A') && (event.target.classList.contains('add-to-cart'))) {
+		console.log(event.target.dataset);
+		event.preventDefault();
+		addToCart(event.target.dataset);	
+	};	
+}
