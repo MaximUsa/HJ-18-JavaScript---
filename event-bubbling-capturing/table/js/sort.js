00d4004ef872671c85fbf0e98018da250cdaa369
Kirill <k.iluschenko@mail.ru>
2018-02-26 20:41:32 +0300

    Задача 2. Сортировка таблицы

--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,27 +1,10 @@
 'use strict';
 
-/*function handleTableClick(event) {
-
-	if (event.target.tagName === 'TH') {
-		event.target.dataset.sortBy = 'qwe';
-
-
-}
-  
-}
-*/
-
 
 function handleTableClick(event) {
-	console.log(event.target.tagName);
 	if (event.target.tagName === 'TH') {
-	
-	(event.target.dataset.dir == '1') ? event.target.dataset.dir = -1 : event.target.dataset.dir = 1; 
-	 
-	 sortTable(event.target.dataset.propName, event.target.dataset.dir);
-	 event.currentTarget.dataset.sortBy = event.target.dataset.propName;
-
-	}
-
-	
+		(event.target.dataset.dir == '1') ? event.target.dataset.dir = -1 : event.target.dataset.dir = 1; 
+		sortTable(event.target.dataset.propName, event.target.dataset.dir);
+		event.currentTarget.dataset.sortBy = event.target.dataset.propName;
+	}	
 }
