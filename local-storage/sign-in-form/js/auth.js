f55e76906826f32406d720e352611e6b86c07f0a
Kirill <k.iluschenko@mail.ru>
2018-02-27 23:16:53 +0300

    Задача 2. Форма авторизации и регистрации

--- local-storage/sign-in-form/js/auth.js
+++ local-storage/sign-in-form/js/auth.js
@@ -5,45 +5,70 @@ const formSignUp = document.querySelector('.sign-up-htm');
 const btnSingIn = formSignIn.querySelector('.button');
 const btnSingUp = formSignUp.querySelector('.button');
 
-const xhr = new XMLHttpRequest();
+const outputSignIn = formSignIn.querySelector('.error-message');
+const outputSignUp = formSignUp.querySelector('.error-message');
 
-xhr.addEventListener('load', (e) => {
-	console.log(xhr.response);
-});
+const xhr = new XMLHttpRequest();
 
 function singIn (event) {
 	let obj =  {}
 	const formData = new FormData(formSignIn);
-	console.log(formData);
 	
-	for (const [k, v] of formData) {
- 		console.log(`${k} : ${v}`);
- 		obj[k] = v
- 		console.log(`${obj[k]}`);
+	for (const [key, value] of formData) {
+ 		obj[key] = value;
 	}
+
 	event.preventDefault();
+	xhr.addEventListener('load', onLoad);
 	xhr.open('POST', 'https://neto-api.herokuapp.com/signin');
 	xhr.setRequestHeader('Content-Type', 'application/json');
 	xhr.send(JSON.stringify(obj));
+
+	function onLoad() {
+		if (xhr.status !== 200) {
+			console.log(`Ответ ${xhr.status}: ${xhr.statusText}`);
+		} else {
+			const response = JSON.parse(xhr.responseText);
+		
+			if (response.error) {
+				outputSignIn.value = response.message;
+			} else {
+				outputSignIn.value = `Пользователь ${response.name} успешно авторизован`;
+			}	
+		}
+	}
 }
 
 function singUp (event) {
 	let obj =  {}
 	const formData = new FormData(formSignUp);
-	console.log(formData);
-	
-	for (const [k, v] of formData) {
- 		console.log(`${k} : ${v}`);
- 		obj[k] = v
- 		console.log(`${obj[k]}`);
-	}
+		
+	for (const [key, value] of formData) {
+ 		obj[key] = value;
+ 	}
 	event.preventDefault();
+	xhr.addEventListener('load', onLoad);
 	xhr.open('POST', 'https://neto-api.herokuapp.com/signup');
 	xhr.setRequestHeader('Content-Type', 'application/json');
 	xhr.send(JSON.stringify(obj));
+
+	function onLoad() {
+		if (xhr.status !== 200) {
+			console.log(`Ответ ${xhr.status}: ${xhr.statusText}`);
+		} else {
+			const response = JSON.parse(xhr.responseText);
+			
+			if (response.error) {
+				outputSignUp.value = response.message;
+			} else {
+				outputSignUp.value = `Пользователь ${response.name} успешно зарегистрирован`;
+			}	
+		}
+	}
 }
 
-btnSingIn.addEventListener('click', singIn)
-btnSingUp.addEventListener('click', singUp)
+btnSingIn.addEventListener('click', singIn);
+btnSingUp.addEventListener('click', singUp);
+
 
 
