d4a0bc3257832f068be960e1b2a8f7e9a6df8de0
Илющенко Кирилл <ika@hosser.org>
2018-02-27 17:44:12 +0300

    Задача 2 /промежуточный вариант/

--- local-storage/sign-in-form/js/auth.js
+++ local-storage/sign-in-form/js/auth.js
@@ -1,1 +1,49 @@
 'use strict';
+
+const formSignIn = document.querySelector('.sign-in-htm');
+const formSignUp = document.querySelector('.sign-up-htm');
+const btnSingIn = formSignIn.querySelector('.button');
+const btnSingUp = formSignUp.querySelector('.button');
+
+const xhr = new XMLHttpRequest();
+
+xhr.addEventListener('load', (e) => {
+	console.log(xhr.response);
+});
+
+function singIn (event) {
+	let obj =  {}
+	const formData = new FormData(formSignIn);
+	console.log(formData);
+	
+	for (const [k, v] of formData) {
+ 		console.log(`${k} : ${v}`);
+ 		obj[k] = v
+ 		console.log(`${obj[k]}`);
+	}
+	event.preventDefault();
+	xhr.open('POST', 'https://neto-api.herokuapp.com/signin');
+	xhr.setRequestHeader('Content-Type', 'application/json');
+	xhr.send(JSON.stringify(obj));
+}
+
+function singUp (event) {
+	let obj =  {}
+	const formData = new FormData(formSignUp);
+	console.log(formData);
+	
+	for (const [k, v] of formData) {
+ 		console.log(`${k} : ${v}`);
+ 		obj[k] = v
+ 		console.log(`${obj[k]}`);
+	}
+	event.preventDefault();
+	xhr.open('POST', 'https://neto-api.herokuapp.com/signup');
+	xhr.setRequestHeader('Content-Type', 'application/json');
+	xhr.send(JSON.stringify(obj));
+}
+
+btnSingIn.addEventListener('click', singIn)
+btnSingUp.addEventListener('click', singUp)
+
+
