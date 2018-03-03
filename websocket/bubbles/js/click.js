4d1f0a72167fcab3b04672cd7f69f2cabe68491c
Kirill <k.iluschenko@mail.ru>
2018-03-03 21:09:43 +0300

    Задача 1.Шарики

--- websocket/bubbles/js/click.js
+++ websocket/bubbles/js/click.js
@@ -1,1 +1,16 @@
 'use strinct';
+
+const body = document.querySelector('body')
+
+const connection = new WebSocket('wss://neto-api.herokuapp.com/mouse');
+
+connection.addEventListener('open', () => {
+	showBubbles(connection);
+});
+
+body.addEventListener('click', event => {
+	connection.send(JSON.stringify({ 
+		x: event.x,
+		y: event.y
+	}));
+})
