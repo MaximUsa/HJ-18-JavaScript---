9a6c6d68af7420c181249fc2452f22c4ac09881a
Илющенко Кирилл <ika@hosser.org>
2018-03-13 14:53:48 +0300

    Задача 3. Страница интернет-магазина

--- dom-from-zero/store/js/dom.js
+++ dom-from-zero/store/js/dom.js
@@ -1,1 +1,24 @@
 'use strict';
+
+function createElement(node) {
+	if (typeof node === 'string') {
+		return document.createTextNode(node);
+	}
+	
+	const element = document.createElement(node.name);
+
+	if (node.props !== null) {
+		Object.keys(node.props).forEach(i => element.setAttribute(i, node.props[i]));
+	}
+	
+	node.childs.forEach(function (item) {
+		if (typeof item === 'string') {
+			element.textContent += item;
+		} else {
+			element.appendChild(createElement(item))
+		}
+	})
+	 
+	return element;
+}
+
